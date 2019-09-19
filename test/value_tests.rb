326f06fc95e8160f690a05e5eb02c49e45e5c60b
David Lynam <dlynam@gmail.com>
2019-09-19 10:13:32 -0700

    fix value tests

--- test/value_tests.rb
+++ test/value_tests.rb
@@ -7,7 +7,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"equal"=>"Search"}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>"Search1", "type"=>"A"}, "_collection"=>"test"}, {:failures=>[{:input_value=>"fail"}], :item=>{"input"=>"Search1", "type"=>"B"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>"Search1", "type"=>"A"}, "_collection"=>"test"}, {:failures=>{:input_value=>"fail"}, :item=>{"input"=>"Search1", "type"=>"B"}, "_collection"=>"test"}]}
     end
 
     it 'should test value equal with regular expression' do
@@ -16,7 +16,7 @@ describe AeEasy::Qa::Validator do
       #validates a single digit as the value
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"regex"=>"^[0-9]$"}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>11}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>11}, "_collection"=>"test"}]}
     end
 
     it 'should test value equal to day of week with regular expression' do
@@ -24,7 +24,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"regex"=>"^(Monday|Tuesday|Wednesday|Thursday|Friday)$"}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>"Wedsss"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>"Wedsss"}, "_collection"=>"test"}]}
     end
 
     it 'should test value equal with if condition' do
@@ -32,7 +32,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"equal"=>"Search", "if"=>{"type"=>{"value"=>"A"}}},"required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>"Search1", "type"=>"A"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>"Search1", "type"=>"A"}, "_collection"=>"test"}]}
     end
 
     it 'should test value less than with if condition' do
@@ -40,7 +40,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"less_than"=>5, "if"=>{"type"=>{"value"=>"A"}}}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>6, "type"=>"A"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>6, "type"=>"A"}, "_collection"=>"test"}]}
     end
 
     it 'should test value more than with if condition' do
@@ -48,7 +48,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"greater_than"=>5, "if"=>{"type"=>{"value"=>"A"}}}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>4, "type"=>"A"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>4, "type"=>"A"}, "_collection"=>"test"}]}
     end
 
     it 'should test comparison of multiple values using or' do
@@ -56,7 +56,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"equal"=>{"or"=>[4,6]}}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>10, "type"=>"B"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>10, "type"=>"B"}, "_collection"=>"test"}]}
     end
 
     it 'should test comparison of multiple values with if condition' do
@@ -64,7 +64,7 @@ describe AeEasy::Qa::Validator do
       qa = AeEasy::Qa::Validator.new(data)
       qa.config = {"individual_validations"=>{"input"=>{"value"=>{"equal"=>{"or"=>[4,6]}, "if"=>{"type"=>{"value"=>"C"}}}, "required"=>true}}}
       results = qa.validate_external([], 'test')
-      assert_equal results, {:errored_items=>[{:failures=>[{:input_value=>"fail"}], :item=>{"input"=>10, "type"=>"C"}, "_collection"=>"test"}]}
+      assert_equal results, {:errored_items=>[{:failures=>{:input_value=>"fail"}, :item=>{"input"=>10, "type"=>"C"}, "_collection"=>"test"}]}
     end
   end
 end
